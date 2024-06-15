11f1d3ca4f448fd48291b29e2c4cc82a842e6959
aroraKamal22 <kamalarora12234@gmail.com>
2024-06-15 15:10:10 +0530

    new

--- assests/js/tilt.js
+++ assests/js/tilt.js
@@ -280,13 +280,3 @@
 
 
 
-
-document.querySelectorAll('.navbar a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        document.getElementById(targetId).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
